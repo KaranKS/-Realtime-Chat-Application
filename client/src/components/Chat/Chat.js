c141e833ae1a27e97505fd730789646257757887
Karan Singh <ksingh1@cs.iitr.ac.in>
2020-10-18 06:31:03 +0530

    creater helper func in server side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () =>{
+let socket;
+
+//here location comes from react router
+const Chat = ({location}) =>{
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT='localhost:5000';
+
+    useEffect( ()=>{
+       const{name,room}=queryString.parse(location.search);
+       
+       socket=io(ENDPOINT);
+       setName(name);
+       setRoom(room);
+       
+       socket.emit('join',{name,room},()=>{
+
+       });
+
+       return ()=>{
+           socket.emit('disconnect');
+           socket.off();
+       }
+
+    },[ENDPOINT,location.search]); //useEffect will activate when value in array will change
+
     return(
         <h1>Chat</h1>
     )
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const socketio=require('socket.io');
 const http=require('http');
 const { create } = require('domain');
+const {addUser,removeUser,getUser,getUserInRoom}=require('./users');
 
 const PORT=process.env.PORT||5000;
 
@@ -12,7 +13,16 @@ const server=http.createServer(app);
 const io=socketio(server);
 
 io.on('connection',(socket)=>{
-    console.log('we have a new connection!!');
+    socket.on('join',({name,room},callback)=>{
+       const{error,user}=addUser({id:socket.id,name,room});
+       if(error) return callback(error);
+
+       socket.emit('message',{user:'admin',text:`${user.name} welcome to the room ${user.room}`});
+       socket.broadcast('message',{user:'admin',text:`${user.name} has joined`});
+       socket.join(user.room);
+
+       callback();
+    });
 
     socket.on('disconnect',()=>{  
         console.log('User had left !!');  
